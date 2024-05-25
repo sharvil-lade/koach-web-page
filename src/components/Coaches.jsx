123f5fb6483fae4bbfa874b5a6e6bd21de0c0643
sharvil-lade <sharvillade9917@gmail.com>
2024-05-25 21:46:12 +0530

    .

--- src/components/Coaches.jsx
+++ src/components/Coaches.jsx
@@ -1,5 +1,5 @@
 // import mentors from "../assets/mentors.png";
-import mentors from "../assets/mentors.png"
+import mentors from "../assets/mentors.png";
 import SlidingImageRl from "./SlidingImageRl";
 
 function Coaches() {
@@ -15,7 +15,7 @@ function Coaches() {
           training required.
         </h2>
         <div className="py-4">
-        <button className="bg-slate-900 text-white py-4 md:py-6 px-6 md:px-16 rounded-lg shadow-lg hover:bg-slate-800 transition duration-300 text-lg md:text-2xl border-2 border-black font-semibold uppercase hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[10px_10px_0px_#3498db] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none">
+          <button className="bg-slate-900 text-white py-4 md:py-6 px-6 md:px-16 rounded-lg shadow-lg hover:bg-slate-800 transition duration-300 text-lg md:text-2xl border-2 border-black font-semibold uppercase hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[10px_10px_0px_#3498db] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none">
             Accelerate Career
           </button>
         </div>
